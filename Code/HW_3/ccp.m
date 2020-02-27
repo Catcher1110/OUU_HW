48378fbdb3d8434bef4ec6082dd6acc71de400d9
root <root@LAPTOP-JC.localdomain>
2020-02-26 23:55:54 -0600

    Modified Big-M Method

--- Code/HW_3/ccp.m
+++ Code/HW_3/ccp.m
@@ -1,38 +1,35 @@
 function [w,b]=ccp(epsilon)
 yalmip clear
-load('data.mat');
+load('data.mat','y','z');
 y = cell2mat(y);
-[m, ] = size(y);
-options = sdpsettings('verbose', 1, 'dualize', 0, 'solver', 'mosek');
-w = sdpvar(m,1);
+options = sdpsettings('verbose', 0, 'dualize', 0, 'solver', 'mosek');
+w = sdpvar(2,1);
 b = sdpvar(1,1);
-t = intvar(N,1);
+g = intvar(40,1);
 obj = 0;
 constraints = {};
 constraints{end + 1} = w <= 100 * ones(2,1);
 constraints{end + 1} = w >= -100 * ones(2,1);
-constraints{end + 1} = w <= 100 * ones(2,1);
-constraints{end + 1} = w >= -100 * ones(2,1);
-constraints{end+1} = b <= 100;
-constraints{end+1} = b >= -100;
-constraints{end + 1} = t >= zeros(N,1);
-constraints{end + 1} = t <= ones(N,1);
-constraints{end + 1} = (w' * y - b)' .* z + 10000 * t  >= ones(N,1);
-constraints{end + 1} = t' * ones(40,1) <= 40 * epsilon;
+constraints{end + 1} = b <= 100;
+constraints{end + 1} = b >= -100;
+constraints{end + 1} = g >= zeros(40,1);
+constraints{end + 1} = g <= ones(40,1);
+constraints{end + 1} = (w' * y - b)' .* z + 100000 * (1-g) >= ones(40,1);
+constraints{end + 1} = g' * ones(40,1) >= 40 * (1-epsilon);
 optimize([constraints{:}], obj, options);
 w = double(w);
 b = double(b);
+figure(1);
+hold on;
 for i = 1:40
     if z(i) == 1
         scatter(y(1,i), y(2, i), 'x', 'r');
-        hold on
     else
         scatter(y(1,i), y(2, i), 'o', 'b');
-        hold on
     end
 end
 x1 = -0.5:0.01:4;
 x2 = (b - w(1) * x1)/w(2);
 plot(x1,x2);
-disp("jj");
+hold off;
 end
